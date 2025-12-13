ebc8f6d45cfba2a8a9bca3d53a2ef2d312fd6466
John M Dusel <jdusel@frederick.edu>
2025-12-13 07:14:54 -0500

    FBK: Maze phase 4

--- src/projects/maze/Main.java
+++ src/projects/maze/Main.java
@@ -9,8 +9,20 @@ public class Main {
             maze.serialize("data/sample_maze_phase1.txt");
     }
 
+    static void phase2() {
+        Maze maze = MazeReader.load("data/hard_maze.txt");
+        if (maze.leadsToExit()) {
+            maze.serialize("data/hard_maze.out");
+        }
+        maze = MazeReader.load("data/hard_maze_nosol.txt");
+        if (maze.leadsToExit()) {
+            maze.serialize("data/hard_maze_nosol.out");
+        }
+    }
+
     public static void main(String[] args) {
         phase1();
+        phase2();
     }
 
 }
